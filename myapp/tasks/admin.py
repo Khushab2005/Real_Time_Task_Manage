73f348aa32eccd0a1f36e19524781bc000f5e2e1
Khushab2005 <khushabch.tagline@gmail.com>
2025-08-07 10:49:45 +0530

    implement Task and Attachment models with admin customization

--- myapp/tasks/admin.py
+++ myapp/tasks/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from myapp.tasks.models import Task, Attachment
+from django.contrib.auth.models import Group
 
-# Register your models here.
+# # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'due_date', 'priority', 'status', 'assigned_to', 'created_by')
+    search_fields = ('title', 'description', )
+    list_filter = ('priority', 'status', 'assigned_to', 'created_by')
+    ordering = ('priority',)
+    list_per_page = 3
+    fieldsets = (
+        ('Task Details', {
+            'fields': ('title', 'description', 'due_date', 'priority', 'status', 'assigned_to', 'created_by')
+        }),
+    )
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('task', 'file', 'created_by',)
+    search_fields = ('task',)
+    list_filter = ('task',)
+    # ordering = ('created_at',)
+    list_per_page = 3
+    fieldsets = (
+        ('Attachment Details', {
+            'fields': ('task', 'file','created_by')
+        }),
+    )
+    
+    
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
+admin.site.unregister(Group)
+
--- myapp/tasks/models.py
+++ myapp/tasks/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from myapp.accounts.models import User
+from myapp.tasks.constants import PriorityChoice , StatusChoice
+import os
 # Create your models here.
+
+# Task Model
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    priority = models.CharField(max_length=15, choices=PriorityChoice, default=PriorityChoice.LOW)
+    status = models.CharField(max_length=15, choices=StatusChoice, default=StatusChoice.PENDING)
+    assigned_to = models.ForeignKey(User, related_name='assigned_tasks', on_delete=models.CASCADE  )
+    created_by = models.ForeignKey(User, related_name='created_tasks', on_delete=models.CASCADE )
+
+    def __str__(self):
+        return self.title
+
+# Upload Path Function for FileField  
+def upload_path(instance, filename):
+    name1 = instance.created_by.name.replace(" ", "_")
+    ext = filename.split('.')[-1]
+    new_filename = f"{name1}.{ext}"
+    return os.path.join("Task_Attachment", name1, new_filename)    
+
+
+# Attachment Model
+class Attachment(models.Model):
+    task = models.ForeignKey(Task, related_name='attachments', on_delete=models.CASCADE)
+    file = models.FileField(upload_to=upload_path, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='attachments_received', on_delete=models.CASCADE)
+
+
