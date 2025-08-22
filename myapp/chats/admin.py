72307595081f753cd0e2928ec887417f16e264ef
Khushab2005 <khushabch.tagline@gmail.com>
2025-08-22 18:26:16 +0530

    Implement chats API , UI and some changes in Model

--- myapp/chats/admin.py
+++ myapp/chats/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from myapp.chats.models import Message,ChatRoom
 
 class ChatRoomAdmin(admin.ModelAdmin):
-    list_display = ('id','name' , 'get_participants')
+    list_display = ('id','room_name' , 'get_participants')
     def get_participants(self, obj):
         return ", ".join([user.email for user in obj.participants.all()])
     
